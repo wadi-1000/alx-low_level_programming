092e2d09d8ea46175d2d85d8a051f4037e2721ac
Joy Kiranga <joykiranga@gmail.com>
2023-08-10 09:57:19 -0700

    Modified 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,6 @@
 #include <stdio.h>
+/**
+ * Main function that takes integer as variable type and holds the puts() function with the input 
+ * Returns 0 if successful an error if not
+ */
 int main ((void) {puts ("Programming is like building a multilingual puzzle\n"); return(0);}
