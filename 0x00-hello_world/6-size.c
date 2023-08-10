6bd22ecf5b32a4be1bc03372a5791c123d4afebf
Joy Kiranga <joykiranga@gmail.com>
2023-08-10 11:05:07 -0700

    Corrected code on 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,7 +7,7 @@ int main(void)
 {
 printf("Size of a char: %zu bytes\n", sizeof(char));
 printf("Size of an int: %zu bytes\n", sizeof(int));
-printf("Size of a long int: %zu\n", sizeof(long int));
-printf("Size of a long long int: %zu\n", sizeof(long long int));
-printf("Size of a float: %zu\n", sizeof(float));
+printf("Size of a long int: %zu bytes\n", sizeof(long int));
+printf("Size of a long long int: %zu bytes\n", sizeof(long long int));
+printf("Size of a float: %zu bytes\n", sizeof(float));
 }
