906757260e8b09987b1db5958dac86776d8af84b
Joy Kiranga <joykiranga@gmail.com>
2023-08-11 09:23:06 -0700

    Created 2-print_alphqbet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Function that  prints the alphabet in lowercase.
+ *
+ * Return: 0 (Successful)
+ */
+int main(void)
+{
+	l = 'a'
+	while (l <= 'z')
+	{
+	putchar(l);
+	l++;
+	}
+	putchar('\n');
+	return (0);
+}
