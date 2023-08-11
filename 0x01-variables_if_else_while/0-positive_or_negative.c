3b6a148b176b6bd0440981341e75f7c5d0bb09e5
Joy Kiranga <joykiranga@gmail.com>
2023-08-11 08:42:41 -0700

    Modified if statement

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,13 +17,13 @@ int main(void)
 	{
 	printf("is positive\n");
 	}
-	else if (n < 0)
+	else if (n == 0)
 	{
-	printf("is negative\n");
+	printf("is zero\n");
 	}
 	else
 	{
-	printf("is zero\n");
+	printf("is negative\n");
 	}
 	return (0);
 }
