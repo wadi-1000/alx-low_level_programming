52b463436d3debeaf5d50d849ae2434711a7e595
Joy Kiranga <joykiranga@gmail.com>
2023-08-12 04:00:51 -0700

    Created 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Function that prints numbers to base 10.
+ *
+ * Return: 0 (Successful)
+ */
+int main(void)
+{
+	int number = 0;
+
+	while (number <= 9)
+	{
+		printf("%d\n", number);
+		number++;
+	}
+	return (0);
+}
