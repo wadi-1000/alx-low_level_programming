3b5feb0abbe36ffe14b2ae4664ff886054b02e65
Joy Kiranga <joykiranga@gmail.com>
2023-08-12 03:46:47 -0700

    Created 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Function that  prints the alphabet in lowercase.
+ *
+ * Return: 0 (Successful)
+ */
+int main(void)
+{
+	char l = 'a';
+
+	while (l <= 'z')
+	{
+		putchar(l);
+		l++;
+	}
+	putchar('\n');
+	return (0);
+}
