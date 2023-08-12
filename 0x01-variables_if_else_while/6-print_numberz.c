ce2d81b23f74253efeecc9ff4e04b70881cd8510
Joy Kiranga <joykiranga@gmail.com>
2023-08-12 05:53:12 -0700

    Created 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Function that prints numbers to base 10.
+ *
+ * Return: 0 (Successful)
+ */
+int main(void)
+{
+	int d;
+
+	for (d = 0; d < 10; d++)
+	}
+	printf("%d\n", d);
+	}
+	return (0);
+}
