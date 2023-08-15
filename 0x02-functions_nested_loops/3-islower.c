f50a4a7a7b2f04c8db85cbfbca64751c108e2fee
Joy Kiranga <joykiranga@gmail.com>
2023-08-15 11:20:22 -0700

    Created 3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _islower - Looks for lowercase letters
+ * @c: The character to be looked for.
+ * Return: 1 for lowercase character, 0 for any other.
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+	return (1);
+	}
+	return (0);
+}
