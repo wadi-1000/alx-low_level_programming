8f41af174c7ad3cac6bdc4b0938b3ee4c53bc9bf
Joy Kiranga <joykiranga@gmail.com>
2023-08-15 13:20:30 -0700

    Created 5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_sign - Prints the sign of a number
+ * @n: The number being checked.
+ *
+ * Return: 1 for a positive -1 for a negative, 0 for any other
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+		{
+			else
+		}
+		_putchar(48);
+		return (0);
+	}
+}
