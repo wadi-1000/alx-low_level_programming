2352331d62ec42001d9e3e2c1fe510f7bd8d13df
Joy Kiranga <joykiranga@gmail.com>
2023-08-15 13:49:50 -0700

    Created 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+ /**
+  * _abs - Gets the absolute value of an integer
+  * @c: The number to be tested.
+  *
+  * Return: Absolute value or zero
+  */
+int _abs(int c)
+{
+	if (c < 0)
+	{
+		int abs_value;
+
+		abs_value = c * -1;
+		return (abs_value);
+	}
+	return (c);
+}
