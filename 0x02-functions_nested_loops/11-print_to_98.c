315f11ee0f6d1d79e12d32db47843f7c5eca8653
Joy Kiranga <joykiranga@gmail.com>
2023-08-16 04:16:21 -0700

    Adhered to betty format

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,34 +9,34 @@
  */
 void print_to_98(int n)
 {
-    if (n <= 98)
-    {
-        for (; n <= 98; n++)
-        {
-            if (n == 98)
-            {
-                printf("%d\n", n);
-                break;
-            }
-            else
-            {
-                printf("%d, ", n);
-            }
-        }
-    }
-    else
-    {
-        for (; n >= 98; n--)
-        {
-            if (n == 98)
-            {
-                printf("%d\n", n);
-                break;
-            }
-            else
-            {
-                printf("%d, ", n);
-            }
-        }
-    }
+	if (n <= 98)
+	{
+		for (; n <= 98; n++)
+		{
+			if (n == 98)
+			{
+				printf("%d\n", n);
+				break;
+			}
+			else
+			{
+				printf("%d, ", n);
+			}
+		}
+	}
+	else
+	{
+		for (; n >= 98; n--)
+		{
+			if (n == 98)
+			{
+				printf("%d\n", n);
+				break;
+			}
+			else
+			{
+				printf("%d, ", n);
+			}
+		}
+	}
 }
