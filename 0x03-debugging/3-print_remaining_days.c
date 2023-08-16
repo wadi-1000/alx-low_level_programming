629b8cc66f74a774d9036d49781aaa05cadde2f3
Joy Kiranga <joykiranga@gmail.com>
2023-08-16 08:29:27 -0700

    Created 3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -10,28 +10,38 @@
 * Return: void
 */
 
-void print_remaining_days(int month, int day, int year)
-{
-	if ((year % 4 == 0) && (year % 400 == || year % 100 != 0))
 
-		if (month >= 3 && day >= 60)
-		{
-			day++;
-		}
 
-	printf("Day of the year: %d\n", day);
-	printf("Remaining days: %d\n", 366 - day);
-		}
-else
+void print_to_98(int n)
 {
-	if (month == 2 && day == 60)
-	{
-		printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
-	}
-	else
-	{
-		printf("Day of the year: %d\n", day);
-		printf("Remaining days: %d\n", 365 - day);
-	}
-}
+    if (n <= 98)
+    {
+        for (; n <= 98; n++)
+        {
+            if (n == 98)
+            {
+                printf("%d\n", n);
+                break;
+            }
+            else
+            {
+                printf("%d, ", n);
+            }
+        }
+    }
+    else
+    {
+        for (; n >= 98; n--)
+        {
+            if (n == 98)
+            {
+                printf("%d\n", n);
+                break;
+            }
+            else
+            {
+                printf("%d, ", n);
+            }
+        }
+    }
 }
