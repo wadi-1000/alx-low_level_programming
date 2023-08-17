f5a3044f38e224f28ae3878ff6633bc1c3e9da9f
Joy Kiranga <joykiranga@gmail.com>
2023-08-17 04:01:25 -0700

    Created 0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isupper - Function that checks for uppercase characters
+ * @c: Character to be checked
+ *
+ * Return: 0 (Successful)
+ *
+ */
+int _isupper(int c)
+{
+	if(c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
