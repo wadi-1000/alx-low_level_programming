eadd31033f77d54218d6af019fa9b10ea0a2731e
Joy Kiranga <joykiranga@gmail.com>
2023-08-17 06:30:42 -0700

    Created 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - Function that multplies 2 integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: 0 (Successful)
+ */
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b
+	return (mul);
+}
