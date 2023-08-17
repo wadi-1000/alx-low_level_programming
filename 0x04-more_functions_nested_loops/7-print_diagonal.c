60f335cb51e1d06dba7f573b89892f6307d3e8c7
Joy Kiranga <joykiranga@gmail.com>
2023-08-17 08:06:52 -0700

    Created 7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_line - Draws a diagonal line
+ *@n: Number of times the character '_' should be printed
+
+ * Return: void
+ */
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n')
+	}
+	else
+	{
+		int spaces = 0;
+		while (n > 0)
+		{
+			int i;
+			for (i = 0; i < spaces; i++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+			spaces++;
+			n--;
+		}
+	}
+}
