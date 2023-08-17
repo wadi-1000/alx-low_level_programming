022d26504c1c1b10efba0fe46666f68a636da59c
Joy Kiranga <joykiranga@gmail.com>
2023-08-17 06:13:08 -0700

    Created 1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * int _isdigit - Function that checks for a digit 0 through 9
+ * @c: The character to be tested
+ *
+ * Return: 0 (Successful)
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (0);
+	}
+}
