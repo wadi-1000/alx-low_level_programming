18b00de62084fbff3ae073d4bacaf6393ec4d00c
Joy Kiranga <joykiranga@gmail.com>
2023-08-17 08:46:46 -0700

    Corrected fucntion description

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -3,29 +3,28 @@
 /**
  *print_line - Draws a diagonal line
  *@n: Number of times the character '_' should be printed
-
  * Return: void
  */
 void print_line(int n)
 {
-	if (n <= 0)
+	if (n  <= 0)
 	{
-		_putchar('\n');
+		putchar('\n');
 	}
 	else
 	{
-		int spaces = 0;
-		while (n > 0)
+		int i, j;
+
+		for (i = 0; i < n; i++)
 		{
-			int i;
-			for (i = 0; i < spaces; i++)
-			{
+			for (j = 0; j < n; j++)
+		{
+			if (j == i)
+				_putchar('\\');
+			else if (j < i)
 				_putchar(' ');
-			}
-			_putchar('\\');
-			_putchar('\n');
-			spaces++;
-			n--;
+		}
+		_putchar('\n');
 		}
 	}
 }
