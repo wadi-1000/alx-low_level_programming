3ad92346aee1ba63830ec84d1d6aeb412c3af341
Joy Kiranga <joykiranga@gmail.com>
2023-08-21 13:09:29 -0700

    Modified function

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,7 +11,7 @@ char *_strcpy(char *dest, char *src)
 	int m = 0;
 	int n = 0;
 
-	while (*(src + m != '\0'))
+	while (*(src + m != '\0')
 	{
 		m++;
 	}
@@ -19,7 +19,7 @@ char *_strcpy(char *dest, char *src)
 	{
 		dest[x] = src[x];
 	}
-	dest[1] = '\0';
+	dest[m] = '\0';
 	return (dest);
 }
 
