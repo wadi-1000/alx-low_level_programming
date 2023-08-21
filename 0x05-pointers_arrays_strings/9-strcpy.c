b4d5ecddd8f878b22c3bcfdc9a761da789cf280f
Joy Kiranga <joykiranga@gmail.com>
2023-08-21 13:06:49 -0700

    Created 9-strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * char *_strcpy - Function that copies pointed string
+ * @dest: destination
+ * @src: source
+ *
+ * Return: string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int m = 0;
+	int n = 0;
+
+	while (*(src + m != '\0'))
+	{
+		m++;
+	}
+	for ( ; n < 1 ; n++)
+	{
+		dest[x] = src[x];
+	}
+	dest[1] = '\0';
+	return (dest);
+}
+
