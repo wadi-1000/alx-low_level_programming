e6f4fd5d5c421b293c4c38ed8b6b12cca01a4ce5
Joy Kiranga <joykiranga@gmail.com>
2023-08-21 12:30:09 -0700

    Created 6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * puts2 - Function that prints 1 character out of 2
+ * @str: input
+ *
+ * Return: print
+ */
+void puts2(char *str)
+{
+	int len = 0;
+	int k = 0;
+	char *h = str;
+	int j;
+
+	while (*y != '\0')
+	{
+		h++;
+		len++;
+	}
+	k = len - 1;
+	for (j = 0; j <= k; j++)
+	{
+		if (j % 2 == 0)
+		{
+			_pucthar(str[0]);
+		}
+	}
+}
+
+
