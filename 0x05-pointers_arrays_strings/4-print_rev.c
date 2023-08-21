e6f17df03e1eff652c6b4b8d31a7652ecd3256b6
Joy Kiranga <joykiranga@gmail.com>
2023-08-21 12:02:37 -0700

    Created 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_rev - Prints string in reverse
+ * @s: string
+ *
+ * Return: 0
+ */
+void print_rev(char *s)
+{
+	int len = 0;
+	int l;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++
+	}
+	s--;
+	for (l = len; l > 0; l--)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
+}
