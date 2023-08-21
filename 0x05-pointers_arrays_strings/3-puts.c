3c9d72c6955ddc4325cb51c8fe54e573a12438be
Joy Kiranga <joykiranga@gmail.com>
2023-08-21 11:54:41 -0700

    Created 3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _puts - Function that prints a string thena new line
+ * @str: string
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str++);
+	}
+	_putchar('\n');
+}
