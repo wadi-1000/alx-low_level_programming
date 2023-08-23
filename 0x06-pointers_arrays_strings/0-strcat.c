508d903b15e2192615270f99b2ed9c8d1ebb370b
Joy Kiranga <joykiranga@gmail.com>
2023-08-23 03:17:35 -0700

    Created 0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strcat - Concats two strings
+ * @dest: Destination string
+ * @src: Source string
+ *
+ * Return: Pointer to the resulting string
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *original_dest = dest;
+
+	while (*dest)
+		dest++;
+	while (*src)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (original_dest);
+}
+
