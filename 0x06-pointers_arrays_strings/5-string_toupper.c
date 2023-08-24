d2e16cfa3830d2b465ccb7281e979def5c674294
Joy Kiranga <joykiranga@gmail.com>
2023-08-24 03:14:12 -0700

    Created 5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * string_toupper - Function that converts string to uppercase
+ * @str: The input string
+ *
+ * Return: A pointer to the modified string
+ */
+char *string_toupper(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - ('a' - 'A');
+		}
+		i++;
+	}
+	return (str);
+}
