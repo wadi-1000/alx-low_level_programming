2019a473442d53d26d4ba95e86556f52567fb843
Joy Kiranga <joykiranga@gmail.com>
2023-08-28 09:30:11 -0700

    Corrected code

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -15,14 +15,13 @@ char *_strstr(char *haystack, char *needle)
 		char *haystack_ptr = haystack;
 		char *needle_ptr = needle;
 
-		while (*haystack_ptr == *needle_ptr && *needle_ptr)
+		while (*haystack_ptr == *needle_ptr && *needle_ptr != '\0')
 		{
 			haystack_ptr++;
 			needle_ptr++;
 		}
 		if (*needle_ptr == '\0')
 			return (haystack);
-		haystack++;
 	}
 	return (NULL);
 }
