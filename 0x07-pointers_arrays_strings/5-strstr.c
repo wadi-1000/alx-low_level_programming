30e614644beba47a6ca6238e8db458ea2cc56935
Joy Kiranga <joykiranga@gmail.com>
2023-08-28 07:45:27 -0700

    Created 5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strstr - Locates a substring
+ * @haystack: The string to search in
+ * @needle: The substring to locate
+ *
+ * Return: A pointer to the beginning of the substring
+ * NULL if the substring is not found
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	while (*haystack)
+	{
+		char *haystack_ptr = haystack;
+		char *needle_ptr = needle;
+
+		while (*haystack_ptr == *needle_ptr && *needle_ptr)
+		{
+			haystack_ptr++;
+			needle_ptr++;
+		}
+		if (*needle_ptr == '\0')
+			return (haystack);
+		haystack++;
+	}
+	return (NULL);
+}
