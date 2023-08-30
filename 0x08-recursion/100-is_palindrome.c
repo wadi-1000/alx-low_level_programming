81c38424ddb196964dce7b1f90fd574fa43b5079
Joy Kiranga <joykiranga@gmail.com>
2023-08-30 10:36:51 -0700

    Created 100-is_palindrome

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * check_palindrome - Recursive function to check if string
+ * is a palidrome
+ * @s: Input string
+ * @start: Strating index
+ * @end: Ending index
+ *
+ * Return: 1 if palindrome else, 0.
+ */
+int check_palindrome(char *s, int start, int end)
+{
+	if (start >= end)
+	{
+		return (1);
+	}
+	if (s[start] != s[end])
+	{
+		return (0);
+	}
+	return (check__palindrome(s, start + 1, end - 1));
+}
