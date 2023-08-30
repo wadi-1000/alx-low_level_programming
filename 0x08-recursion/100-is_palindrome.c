561bfbaa04e78c07b890242a0703f0fac2f86943
Joy Kiranga <joykiranga@gmail.com>
2023-08-30 11:16:20 -0700

    Created 101-wildcmpc

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -19,5 +19,28 @@ int check_palindrome(char *s, int start, int end)
 	{
 		return (0);
 	}
-	return check_palindrome(s, start + 1, end - 1);
+	return (check_palindrome(s, start + 1, end - 1));
+}
+
+/**
+ * is_palindrome - Function that returns 1 if a string is a palindrome
+ * and 0 if not.
+ * @s: Input string
+ *
+ * Return: 1 if palindrome else, 0.
+ */
+int is_palindrome(char *s)
+{
+	int len = 0;
+	int i = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	if (len == 0)
+	{
+		return (1);
+	}
+	return (check_palindrome(s, i, len - 1));
 }
