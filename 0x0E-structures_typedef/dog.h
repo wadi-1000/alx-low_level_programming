ae74cda774ac8c72925940f0a1c645489f893886
Joy Kiranga <joykiranga@gmail.com>
2023-09-12 07:34:12 -0700

    Re-wrote function

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef _HEADER_
-#define _HEADER_
+#ifndef DOG_H
+#define DOG_H
 
 /**
  * struct dog - Dog attributes
@@ -18,10 +18,11 @@ struct dog
 /**
  * dog_t - Typedef for dog structure
  */
-typedef struct dog dog_t;
 
-void init_dog(struct * d, char *name, float age, char *owner);
+void init_dog(struct dog * d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
 #endif
