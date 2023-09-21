debcb8179d315a7b31d28253cdb2c0e744079b74
Joy Kiranga <joykiranga@gmail.com>
2023-09-21 10:55:33 -0700

    Corrected code to betty format

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -4,6 +4,7 @@
 #include <stddef.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 
 /**
  * struct list_s - a singly linked list
@@ -19,11 +20,11 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
-int putchar(char c);
+int putchar(int c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
-#endif 
+#endif
