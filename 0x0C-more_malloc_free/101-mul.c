ec7ab1e4b33c7165d01667ce85fc93d6a25e805e
Joy Kiranga <joykiranga@gmail.com>
2023-09-21 11:42:19 -0700

    Created 3-add_node_end.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -28,19 +28,39 @@ int multiply(int num1, int num2);
  */
 int main(int argc, char *argv[])
 {
-	int num1, num2;
+    int num1, num2, result;
 
-	if (argc != 3)
-	{
-		fprintf(stderr, "Error\n");
-		return (98);
-	}
+    if (argc != 3)
+    {
+        return (98);
+    }
 
-	num1 = str_to_int(argv[1]);
-	num2 = str_to_int(argv[2]);
+    num1 = str_to_int(argv[1]);
+    num2 = str_to_int(argv[2]);
+    result = multiply(num1, num2);
 
-	printf("%d\n", multiply(num1, num2));
-	return (0);
+    char result_str[12]; /* Maximum digits for a 32-bit integer */
+    int i = 0;
+    while (result != 0)
+    {
+        result_str[i++] = (result % 10) + '0';
+        result /= 10;
+    }
+    if (i == 0)
+    {
+        result_str[i++] = '0';
+    }
+    result_str[i] = '\0';
+
+    i--;
+    while (i >= 0)
+    {
+        putchar(result_str[i]);
+        i--;
+    }
+    putchar('\n');
+
+    return (0);
 }
 
 /**
@@ -51,26 +71,26 @@ int main(int argc, char *argv[])
  */
 int str_to_int(const char *str)
 {
-	int result = 0;
-	int sign = 1;
-	int i = 0;
+    int result = 0;
+    int sign = 1;
+    int i = 0;
 
-	if (str[0] == '-')
-	{
-		sign = -1;
-		i = 1;
-	}
+    if (str[0] == '-')
+    {
+        sign = -1;
+        i = 1;
+    }
 
-	for (; str[i] != '\0'; i++)
-	{
-		if (!isdigit(str[i]))
-		{
-			fprintf(stderr, "Error\n");
-			exit(98);
-		}
-		result = result * 10 + (str[i] - '0');
-	}
-	return (result * sign);
+    for (; str[i] != '\0'; i++)
+    {
+        if (!isdigit(str[i]))
+        {
+            return (98);
+        }
+        result = result * 10 + (str[i] - '0');
+    }
+
+    return (result * sign);
 }
 
 /**
@@ -82,6 +102,6 @@ int str_to_int(const char *str)
  */
 int multiply(int num1, int num2)
 {
-	return (num1 * num2);
+    return (num1 * num2);
 }
 
