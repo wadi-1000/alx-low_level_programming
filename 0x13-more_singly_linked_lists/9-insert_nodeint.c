0a908127db03858e8038ae66e8e421427fe38f3b
Joy Kiranga <joykiranga@gmail.com>
2023-09-26 10:21:57 -0700

    Corrected betty documentation

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * listint_t *insert_nodeint_at_index - inserts a new node at a given position.
+ * insert_nodeint_at_index - inserts a new node at a given position.
  * @head: pointer to the first node in the list
  * @idx: index where the new node is added
  * @n: data to insert in the new node
