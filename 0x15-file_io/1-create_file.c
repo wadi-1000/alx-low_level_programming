463cdb02057e5b7a4e3b70235fc6ffd857808bba
Joy Kiranga <joykiranga@gmail.com>
2023-10-02 10:54:11 -0700

    Changed brackets and corrected typo

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,18 +15,18 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	file_f = open(filename, O_CREAT | O_WRONGLY | O_TRUNC, 0600);
+	file_f = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (file_f == -1)
 		return (-1);
 	if (!text_content)
 		text_content = "";
 
-	for (cletters = 0; text_content(cletters); cletters;
-			rwr = write(file_f, text_content, cletters);
-
-			if (rwr == -1)
-				return (-1);
-			close(file_f);
-			return (1);
+	for (cletters = 0; text_content[cletters]; cletters++);
+	rwr = write(file_f, text_content, cletters);
+	
+	if (rwr == -1)
+		return (-1);
+	close(file_f);
+	return (1);
 }
