13fc22ff0085b19592964011d454049e7f3503dc
Joy Kiranga <joykiranga@gmail.com>
2023-10-02 10:58:21 -0700

    Corrected to betty format

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,9 +22,10 @@ int create_file(const char *filename, char *text_content)
 	if (!text_content)
 		text_content = "";
 
-	for (cletters = 0; text_content[cletters]; cletters++);
+	for (cletters = 0; text_content[cletters]; cletters++)
+		;
 	rwr = write(file_f, text_content, cletters);
-	
+
 	if (rwr == -1)
 		return (-1);
 	close(file_f);
